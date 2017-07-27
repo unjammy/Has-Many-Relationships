e02735b1b4e3850744c9b4b4fa2024adb2e6f286
clarkdenunzio <clark@clarkdenunzio.com>
2017-07-26 21:58:11 -1000

    making tables

--- has_many_blogs.sql
+++ has_many_blogs.sql
@@ -7,18 +7,21 @@ CREATE DATABASE has_many_blogs WITH OWNER has_many_user;
 \connect has_many_blogs
 
 CREATE TABLE users (
-
-id SERIAL NOT NULL,
-username VARCHAR(90) NOT NULL,
-first_name VARCHAR(90) DEFAULT NULL,
-last_name VARCHAR(90) DEFAULT NULL,
-created_at time with timezone NOT NULL DEFAULT now(),
-updated_at time with timezone NOT NULL DEFAULT now()
+  id SERIAL NOT NULL,
+  username VARCHAR(90) NOT NULL,
+  first_name VARCHAR(90) DEFAULT NULL,
+  last_name VARCHAR(90) DEFAULT NULL,
+  created_at time NOT NULL DEFAULT now(),
+  updated_at time NOT NULL DEFAULT now()
 );
 
 CREATE TABLE posts(
-id SERIAL NOT NULL,
-
+  id SERIAL NOT NULL,
+  title VARCHAR(180) DEFAULT NULL,
+  url VARCHAR(510) DEFAULT NULL,
+  content TEXT DEFAULT NULL,
+  created_at time NOT NULL DEFAULT now(),
+  updated_at time NOT NULL DEFAULT now()
 );
 
 \i scripts/blog_data.sql
