3c1de663f1b39a98a6c09aae8c06458b74e35135
clarkdenunzio <clark@clarkdenunzio.com>
2017-07-27 21:57:41 -1000

    number 13 complete

--- joins.sql
+++ joins.sql
@@ -36,3 +36,11 @@ WHERE comments.body LIKE '%USB%';
 SELECT posts.title AS "post_title", users.first_name, users.last_name, comments.body AS "comment_body"
 FROM comments INNER JOIN users ON comments.user_id = users.id INNER JOIN posts ON comments.post_id = posts.id
 WHERE comments.body LIKE '%matrix%';
+
+SELECT users.first_name, users.last_name, comments.body AS "comment_body"
+FROM posts INNER JOIN users ON posts.user_id = users.id INNER JOIN comments ON posts.id = comments.post_id
+WHERE comments.body LIKE '%SSL%' AND posts.content LIKE '%dolorum%';
+
+SELECT users.first_name AS "post_author_first_name", users.last_name AS "post_author_last_name", posts.title AS "post_title", users.username AS "comment_author_username", comments.body AS "comment_body"
+FROM posts INNER JOIN users ON posts.user_id = users.id INNER JOIN comments ON posts.id = comments.post_id
+WHERE comments.body LIKE ANY(ARRAY['%SSL%', '%firewall%']) AND posts.content LIKE '%nemo%';
